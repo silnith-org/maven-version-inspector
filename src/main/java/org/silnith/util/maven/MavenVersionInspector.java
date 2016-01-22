44670cad096bffc6ae0c794d2f126789fdd8c9c2
kent <silnith@gmail.com>
2016-01-21 23:58:42 -0800

    Clean up the source code a bit.

--- src/main/java/org/silnith/util/maven/MavenVersionInspector.java
+++ src/main/java/org/silnith/util/maven/MavenVersionInspector.java
@@ -17,74 +17,79 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 public class MavenVersionInspector {
-
-	public static final String GROUP_ID = "groupId";
-
-	public static final String ARTIFACT_ID = "artifactId";
-
-	public static final String VERSION = "version";
-
-	public static void main(final String[] args) throws IOException {
-		final MavenVersionInspector inspector = new MavenVersionInspector();
-		inspector.trialLoad();
-	}
-
-	public void trialLoad() throws IOException {
-		final String prefix = "META-INF/maven/";
-		System.out.println(prefix);
-		final Enumeration<URL> resources = MavenVersionInspector.class.getClassLoader().getResources(prefix);
-		while (resources.hasMoreElements()) {
-			final URL url = resources.nextElement();
-			System.out.println(url);
-		}
-	}
-
-	public Collection<Properties> loadVersions() throws IOException {
-		final PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-		final Resource[] resources = resolver.getResources("classpath*:/META-INF/maven/**/pom.properties");
-		
-		final ArrayList<Properties> versionProperties = new ArrayList<Properties>(resources.length);
-		
-		for (final Resource resource : resources) {
-			final Properties properties = new Properties();
-			
-			properties.load(resource.getInputStream());
-			
-			versionProperties.add(properties);
-		}
-		
-		return versionProperties;
-	}
-
-	public SortedMap<String, SortedMap<String, SortedSet<ArtifactVersion>>> sortVersions(final Collection<Properties> versionProperties) {
-		final TreeMap<String, SortedMap<String, SortedSet<ArtifactVersion>>> groupIdMap = new TreeMap<String, SortedMap<String, SortedSet<ArtifactVersion>>>();
-		
-		for (final Properties properties : versionProperties) {
-			final String groupId = properties.getProperty(GROUP_ID);
-			final String artifactId = properties.getProperty(ARTIFACT_ID);
-			final String version = properties.getProperty(VERSION);
-			final ArtifactVersion artifactVersion = new DefaultArtifactVersion(version);
-			
-			final SortedMap<String, SortedSet<ArtifactVersion>> artifactIdVersions;
-			if (groupIdMap.containsKey(groupId)) {
-				artifactIdVersions = groupIdMap.get(groupId);
-			} else {
-				artifactIdVersions = new TreeMap<String, SortedSet<ArtifactVersion>>();
-				groupIdMap.put(groupId, artifactIdVersions);
-			}
-			
-			final SortedSet<ArtifactVersion> versions;
-			if (artifactIdVersions.containsKey(artifactId)) {
-				versions = artifactIdVersions.get(artifactId);
-			} else {
-				versions = new TreeSet<ArtifactVersion>();
-				artifactIdVersions.put(artifactId, versions);
-			}
-			
-			versions.add(artifactVersion);
-		}
-		
-		return groupIdMap;
-	}
-
+    
+    public static final String GROUP_ID = "groupId";
+    
+    public static final String ARTIFACT_ID = "artifactId";
+    
+    public static final String VERSION = "version";
+    
+    public static void main(final String[] args) throws IOException {
+        final MavenVersionInspector inspector = new MavenVersionInspector();
+        inspector.trialLoad();
+    }
+    
+    public void trialLoad() throws IOException {
+        final String prefix = "META-INF/maven/";
+        System.out.println(prefix);
+        final Enumeration<URL> resources = MavenVersionInspector.class
+                .getClassLoader().getResources(prefix);
+        while (resources.hasMoreElements()) {
+            final URL url = resources.nextElement();
+            System.out.println(url);
+        }
+    }
+    
+    public Collection<Properties> loadVersions() throws IOException {
+        final PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        final Resource[] resources = resolver
+                .getResources("classpath*:/META-INF/maven/**/pom.properties");
+        
+        final ArrayList<Properties> versionProperties = new ArrayList<Properties>(
+                resources.length);
+        
+        for (final Resource resource : resources) {
+            final Properties properties = new Properties();
+            
+            properties.load(resource.getInputStream());
+            
+            versionProperties.add(properties);
+        }
+        
+        return versionProperties;
+    }
+    
+    public SortedMap<String, SortedMap<String, SortedSet<ArtifactVersion>>> sortVersions(
+            final Collection<Properties> versionProperties) {
+        final TreeMap<String, SortedMap<String, SortedSet<ArtifactVersion>>> groupIdMap = new TreeMap<String, SortedMap<String, SortedSet<ArtifactVersion>>>();
+        
+        for (final Properties properties : versionProperties) {
+            final String groupId = properties.getProperty(GROUP_ID);
+            final String artifactId = properties.getProperty(ARTIFACT_ID);
+            final String version = properties.getProperty(VERSION);
+            final ArtifactVersion artifactVersion = new DefaultArtifactVersion(
+                    version);
+            
+            final SortedMap<String, SortedSet<ArtifactVersion>> artifactIdVersions;
+            if (groupIdMap.containsKey(groupId)) {
+                artifactIdVersions = groupIdMap.get(groupId);
+            } else {
+                artifactIdVersions = new TreeMap<String, SortedSet<ArtifactVersion>>();
+                groupIdMap.put(groupId, artifactIdVersions);
+            }
+            
+            final SortedSet<ArtifactVersion> versions;
+            if (artifactIdVersions.containsKey(artifactId)) {
+                versions = artifactIdVersions.get(artifactId);
+            } else {
+                versions = new TreeSet<ArtifactVersion>();
+                artifactIdVersions.put(artifactId, versions);
+            }
+            
+            versions.add(artifactVersion);
+        }
+        
+        return groupIdMap;
+    }
+    
 }
--- src/test/java/org/silnith/util/maven/MavenVersionInspectorTest.java
+++ src/test/java/org/silnith/util/maven/MavenVersionInspectorTest.java
@@ -1,6 +1,8 @@
 package org.silnith.util.maven;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,52 +18,57 @@ import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.junit.Test;
 
 public class MavenVersionInspectorTest {
-
-	private MavenVersionInspector mavenVersionInspector = new MavenVersionInspector();
-
-	@Test
-	public void testLoadVersions() throws IOException {
-		final Collection<Properties> versionProperties = mavenVersionInspector.loadVersions();
-		
-		assertNotNull(versionProperties);
-		assertFalse(versionProperties.isEmpty());
-	}
-
-	@Test
-	public void testSortVersions() {
-		final Properties properties102 = new Properties();
-		properties102.put("groupId", "org.silnith");
-		properties102.put("artifactId", "maven-version-inspector");
-		properties102.put("version", "1.0.2");
-		final Properties properties103 = new Properties();
-		properties103.put("groupId", "org.silnith");
-		properties103.put("artifactId", "maven-version-inspector");
-		properties103.put("version", "1.0.3-SNAPSHOT");
-		final Properties properties098 = new Properties();
-		properties098.put("groupId", "org.silnith");
-		properties098.put("artifactId", "maven-version-inspector");
-		properties098.put("version", "0.9.8-SNAPSHOT");
-		final ArrayList<Properties> properties = new ArrayList<Properties>();
-		properties.add(properties102);
-		properties.add(properties103);
-		properties.add(properties098);
-		
-		final SortedMap<String, SortedMap<String, SortedSet<ArtifactVersion>>> sortedVersions = mavenVersionInspector.sortVersions(properties);
-		
-		assertNotNull(sortedVersions);
-		
-		final TreeSet<ArtifactVersion> mavenVersionInspectorVersions = new TreeSet<ArtifactVersion>();
-		mavenVersionInspectorVersions.add(new DefaultArtifactVersion("0.9.8-SNAPSHOT"));
-		mavenVersionInspectorVersions.add(new DefaultArtifactVersion("1.0.2"));
-		mavenVersionInspectorVersions.add(new DefaultArtifactVersion("1.0.3-SNAPSHOT"));
-		
-		final TreeMap<String, SortedSet<ArtifactVersion>> artifactVersionMap = new TreeMap<String, SortedSet<ArtifactVersion>>();
-		artifactVersionMap.put("maven-version-inspector", mavenVersionInspectorVersions);
-		
-		final SortedMap<String, SortedMap<String, SortedSet<ArtifactVersion>>> expectedSortedVersions = new TreeMap<String, SortedMap<String, SortedSet<ArtifactVersion>>>();
-		expectedSortedVersions.put("org.silnith", artifactVersionMap);
-		
-		assertEquals(expectedSortedVersions, sortedVersions);
-	}
-
+    
+    private final MavenVersionInspector mavenVersionInspector = new MavenVersionInspector();
+    
+    @Test
+    public void testLoadVersions() throws IOException {
+        final Collection<Properties> versionProperties = mavenVersionInspector
+                .loadVersions();
+        
+        assertNotNull(versionProperties);
+        assertFalse(versionProperties.isEmpty());
+    }
+    
+    @Test
+    public void testSortVersions() {
+        final Properties properties102 = new Properties();
+        properties102.put("groupId", "org.silnith");
+        properties102.put("artifactId", "maven-version-inspector");
+        properties102.put("version", "1.0.2");
+        final Properties properties103 = new Properties();
+        properties103.put("groupId", "org.silnith");
+        properties103.put("artifactId", "maven-version-inspector");
+        properties103.put("version", "1.0.3-SNAPSHOT");
+        final Properties properties098 = new Properties();
+        properties098.put("groupId", "org.silnith");
+        properties098.put("artifactId", "maven-version-inspector");
+        properties098.put("version", "0.9.8-SNAPSHOT");
+        final ArrayList<Properties> properties = new ArrayList<Properties>();
+        properties.add(properties102);
+        properties.add(properties103);
+        properties.add(properties098);
+        
+        final SortedMap<String, SortedMap<String, SortedSet<ArtifactVersion>>> sortedVersions = mavenVersionInspector
+                .sortVersions(properties);
+        
+        assertNotNull(sortedVersions);
+        
+        final TreeSet<ArtifactVersion> mavenVersionInspectorVersions = new TreeSet<ArtifactVersion>();
+        mavenVersionInspectorVersions.add(new DefaultArtifactVersion(
+                "0.9.8-SNAPSHOT"));
+        mavenVersionInspectorVersions.add(new DefaultArtifactVersion("1.0.2"));
+        mavenVersionInspectorVersions.add(new DefaultArtifactVersion(
+                "1.0.3-SNAPSHOT"));
+        
+        final TreeMap<String, SortedSet<ArtifactVersion>> artifactVersionMap = new TreeMap<String, SortedSet<ArtifactVersion>>();
+        artifactVersionMap.put("maven-version-inspector",
+                mavenVersionInspectorVersions);
+        
+        final SortedMap<String, SortedMap<String, SortedSet<ArtifactVersion>>> expectedSortedVersions = new TreeMap<String, SortedMap<String, SortedSet<ArtifactVersion>>>();
+        expectedSortedVersions.put("org.silnith", artifactVersionMap);
+        
+        assertEquals(expectedSortedVersions, sortedVersions);
+    }
+    
 }
